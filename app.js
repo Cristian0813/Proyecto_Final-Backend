d38807f4cfac9c6651dbfa9b7d9390efab9093c1
Cristian Arias <javier.0813@hotmail.com>
2023-12-15 11:16:58 -0500

    Ajustar la aplicacion al escuchar en 0.0.0.0

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-require('dotenv').config();
-
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const apiRoutes = require('./routes/apiRoutes');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
-
-// view engine setup
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -18,19 +15,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 
-const mongoConnect = async ()=>{
+const mongoConnect = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGODB_URL
-    )
-    console.log('Aplicación conectada al puerto ' + process.env.PORT)
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log('Aplicación conectada al puerto ' + process.env.PORT);
   } catch (err) {
-    console.log(err)
-    process.exit(1)
+    console.log(err);
+    process.exit(1);
   }
-}
+};
 
 app.use('/api', apiRoutes);
+
+// Escuchar en 0.0.0.0: process.env.PORT
+const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Aplicación escuchando en http://0.0.0.0:${port}`);
+});
 
 mongoConnect();
 module.exports = app;
